7708bff4aa812a5cc8f905189b2c66be274e3507
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-03-16 23:37:59 +0530

    Half Services was Ready in this time

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import './Services.css'
+import React from "react";
+import "./Services.css";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
 
 function Services() {
-  return (
-    <div className="services">
-        {/* Left side */}
-        <div className="awesome">
-
+    return (
+        <div className="services">
+            {/* Left side */}
+            <div className="awesome">
+              <span>My Awesome</span>
+              <span>Services</span>
+              <span>
+              I'm a Web development: As a front-end and back-end developer
+              <br />
+              Java programmer for competitive programming
+              </span>
+              <button className="button s-button">Download CV</button>
+              <div className="s-blur1 blur" style={{background: "#ABF1FF94"}}></div>
+            </div>
+            {/* Right side */}
+            <div className="cards">
+              I am the Right Side
+            </div>
         </div>
-        {/* Right side */}
-        <div className="cards">
-
-        </div>
-    </div>
-  )
+    );
 }
 
-export default Services
+export default Services;
