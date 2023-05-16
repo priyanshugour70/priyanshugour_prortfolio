0b770b4a3c58ab8d8663d235289ba9590e640856
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-05-16 15:33:58 +0530

    Some Chages

--- portfolio.ts
+++ portfolio.ts
@@ -84,11 +84,11 @@ export const skillsSection: SkillsSectionType = {
           fontAwesomeClassName: 'logos:redux',
         },
         {
-          skillName: 'NPM',
+          skillName: 'npm',
           fontAwesomeClassName: 'logos:npm-icon',
         },
         {
-          skillName: 'Yarn',
+          skillName: 'yarn',
           fontAwesomeClassName: 'logos:yarn',
         },
       ],
@@ -149,47 +149,6 @@ export const skillsSection: SkillsSectionType = {
         },
       ],
     },
-    // {
-    //   title: 'Blockchain',
-    //   lottieAnimationFile: '/lottie/skills/ethereum.json', // Path of Lottie Animation JSON File
-    //   skills: [
-    //     emoji(
-    //       '⚡ Experience in developing Smart Contract using Solidity & Ethereum'
-    //     ),
-    //     emoji(
-    //       '⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura'
-    //     ),
-    //     emoji(
-    //       '⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles'
-    //     ),
-    //     emoji('⚡ Developing NFT Smart Contracts using ERC-721 Token Standard'),
-    //     emoji(
-    //       '⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS'
-    //     ),
-    //   ],
-    //   softwareSkills: [
-    //     {
-    //       skillName: 'Ethereum',
-    //       fontAwesomeClassName: 'logos:ethereum',
-    //     },
-    //     {
-    //       skillName: 'Solidity',
-    //       fontAwesomeClassName: 'logos:solidity',
-    //     },
-    //     {
-    //       skillName: 'Web3js',
-    //       fontAwesomeClassName: 'logos:web3js',
-    //     },
-    //     {
-    //       skillName: 'Metamask',
-    //       fontAwesomeClassName: 'logos:metamask-icon',
-    //     },
-    //     {
-    //       skillName: 'Ganache',
-    //       fontAwesomeClassName: 'logos:ganache-icon',
-    //     },
-    //   ],
-    // },
   ],
 };
 
@@ -240,56 +199,16 @@ export const experience: ExperienceType[] = [
   //   companyLogo: '/img/icons/common/dusecaSoftware.jpg',
   //   date: 'Apr 2022 – Jun 2022',
   //   desc: 'I worked as frontend web developer to design web based admin dashboards for mulitple apps using Django, Bootstrap, jQuery, Chart.js and some other libraries.',
-  // },
-  // {
-  //   role: 'API Engineer',
-  //   company: 'Duseca Software',
-  //   companyLogo: '/img/icons/common/dusecaSoftware.jpg',
-  //   date: 'Jan 2022 – Mar 2022',
-  //   desc: 'I worked as API Engineer on a Kale App. Kale is a community platform for the creation of social reviews based on verified transactions. I built Fully Optimized and High Performance RESTful APIs using Django, DjangoRESTFramework, PostgresSQL, JWT Authentication, AWS, Integrated Plaid APIs for Fetching Bank Transactions, Integrated Sentry for monitoring Django Server and deployed the API on Heroku Server.',
-  //   // descBullets: [
-  //   // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-  //   // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-  //   // ],
-  // },
-  // {
-  //   role: 'Full Stack Developer',
-  //   company: 'Bleed-AI',
-  //   companyLogo: '/img/icons/common/bleedAI.jpg',
-  //   date: 'Sept 2021 - Oct 2021',
-  //   desc: 'Worked as django web developer to create a website in which I integrated computer vision AI model (built by the team of BleedAI) to process YouTube URLs and show Real Time processing on Website using Django Channels WebSockets , threading for running multiple instances of AI Model, jQuery for UI rendering and deployed it on Heroku server.',
-  // },
-  // {
-  //   role: 'Backend Developer',
-  //   company: 'Wapidu',
-  //   companyLogo: '/img/icons/common/wapidu.jpg',
-  //   date: 'Sept 2021',
-  //   desc: 'Worked as a Django Developer to integrate Stripe payment gateway in wapidu.com and Created REST APIs using Django REST Framework to integrate Django Stripe backend to Vue.js Frontend and Deployed it on Azure based Docker container registry.',
-  // },
+  // }
 ];
 
 export const projects: ProjectType[] = [
-  // {
-  //   name: 'developer-portfolio',
-  //   desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
-  //   github: 'https://github.com/1hanzla100/developer-portfolio',
-  //   link: 'https://developer-portfolio-1hanzla100.vercel.app/',
-  // },
-  // {
-  //   name: 'AtlasMart',
-  //   desc: 'With Atlas Mart, it’s easy to find the products with the best price and ship them straight to your door.',
-  //   github: 'https://github.com/1hanzla100/Django-React-Marketplace',
-  // },
-  // {
-  //   name: 'Technota (Forum)',
-  //   desc: 'Get hands-on experience in technical skills with Technota',
-  //   github: 'https://github.com/1hanzla100/django-react-forum',
-  // },
-  // {
-  //   name: 'Shopaza (Ecommerce)',
-  //   desc: 'Now your reach to all your favorite entertainment and luxury items is just one touch apart',
-  //   github: 'https://github.com/1hanzla100/Django-ecommerce',
-  // },
+  {
+    name: 'priyanshu-portfolio',
+    desc: 'Software Developer Portfolio Template built with react.js and next.js bootstrap that helps you showcase your work and skills as a software developer.',
+    github: 'https://github.com/priyanshugour70/priyanshugour_prortfolio',
+    link: 'https://priyanshugour70.vercel.app/',
+  },
 ];
 
 export const feedbacks: FeedbackType[] = [
@@ -297,12 +216,7 @@ export const feedbacks: FeedbackType[] = [
   //   name: 'John Smith',
   //   feedback:
   //     'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
-  // },
-  // {
-  //   name: 'John Smith',
-  //   feedback:
-  //     'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.',
-  // },
+  // }
 ];
 
 // option to hide or show the ContactUs component
@@ -314,8 +228,8 @@ export const seoData: SEODataType = {
   description:
     'A passionate Full Stack Web Developer and Java Developer.',
   author: 'Priyanshu Gour',
-  image: 'https://avatars3.githubusercontent.com/u/59178380?v=4',
-  url: 'https://developer-portfolio-1hanzla100.vercel.app',
+  image: 'https://avatars.githubusercontent.com/u/111908314?s=400&u=7a933bd6e2577892cbb48edc0a634f5355712e95&v=4',
+  url: 'https://priyanshugour70.vercel.app',
   keywords: [
     'Priyanshu',
     'Priyanshu Gour',
@@ -324,5 +238,20 @@ export const seoData: SEODataType = {
     'Portfolio',
     'priyanshu Portfolio ',
     'priyanshu gour Portfolio',
+    'portfolio',
+    'priyanshu',
+    'gour',
+    'priyanshugour70',
+    'developer',
+    'developer portfolio',
+    'nextjs portfolio',
+    'nextjs developer portfolio',
+    'nextjs priyanshu gour',
+    'priyanshu gour versel',
+    'priyanshu gour instagram',
+    'priyanshu gour github',
+    'priyanshu gour linkedin',
+    'priyanshu gour hackerearth',
+    'priyanshu gour leetcode'
   ],
 };
